ca06ba21b6fc1959b183905713e52caf6734ace5
Sallai András <andrewteki@gmail.com>
2021-10-02 10:45:22 +0200

    Init

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './components/Home';
+import { navigation } from '@react-navigation/native';
 
 export default function App() {
+  function onHandler() {
+    console.log('Valami történik');
+    navigation.navigate('Home');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Tartalom</Text>
+      <Text>Másik</Text>
+      <Text onPress={onHandler}>Harmadik</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  text: {
+    fontSize: 32,
+  }
 });
